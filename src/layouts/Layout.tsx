04fad4060b8ff82a3e38c4064a3168911013d36d
Doooodie <unfair.slenderman@gmail.com>
2023-05-10 22:44:09 +0200

    feat: add Inter font, implement it in theme

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
+import customTheme from 'utils/customTheme';
 import Header from './Header';
 
 function Layout() {
   return (
-    <MantineProvider withNormalizeCSS withGlobalStyles>
+    <MantineProvider theme={customTheme} withNormalizeCSS withGlobalStyles>
       <Header />
       <Outlet />
     </MantineProvider>
---
+++ src/utils/customTheme.ts
@@ -0,0 +1,12 @@
+import { MantineThemeOverride } from '@mantine/core';
+
+const FONT = 'Inter, sans-serif';
+
+const customTheme: MantineThemeOverride = {
+  fontFamily: FONT,
+  headings: {
+    fontFamily: FONT,
+  },
+};
+
+export default customTheme;
