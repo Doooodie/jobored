7316400dc838b5315432f44561093047466b0e91
Doooodie <unfair.slenderman@gmail.com>
2023-05-15 21:06:36 +0200

    feat: add header ui and colors to customTheme

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,5 +1,57 @@
+import { Link, useLocation } from 'react-router-dom';
+import {
+  Header as MantineHeader,
+  Container,
+  Group,
+  Image,
+  Title,
+  Flex,
+  Anchor,
+  SimpleGrid,
+} from '@mantine/core';
+import logo from 'assets/logo.svg';
+
 function Header() {
-  return <h1>Хедер</h1>;
+  const { pathname } = useLocation();
+  const isHome = pathname === '/';
+
+  return (
+    <MantineHeader height={84}>
+      <Container h='100%'>
+        <SimpleGrid
+          h='100%'
+          cols={3}
+          breakpoints={[
+            { maxWidth: '48rem', cols: 2 },
+            { maxWidth: '36rem', cols: 1 },
+          ]}
+          sx={{
+            alignItems: 'center',
+            '@media (max-width: 36rem)': {
+              gridTemplateColumns: 'auto',
+              justifyItems: 'center',
+            },
+          }}
+        >
+          <Group spacing={12} noWrap>
+            <Image src={logo} width={30} alt='Jobored logo' />
+            <Title order={2} sx={{ fontFamily: 'Poppins, sans-serif', cursor: 'default' }}>
+              Jobored
+            </Title>
+          </Group>
+
+          <Flex justify='space-between' wrap='wrap' gap={20} maw={273}>
+            <Anchor component={Link} to='/' c={isHome ? 'blue' : ''}>
+              Поиск Вакансий
+            </Anchor>
+            <Anchor component={Link} to='/favorites' c={!isHome ? 'blue' : ''}>
+              Избранное
+            </Anchor>
+          </Flex>
+        </SimpleGrid>
+      </Container>
+    </MantineHeader>
+  );
 }
 
 export default Header;
--- src/utils/customTheme.ts
+++ src/utils/customTheme.ts
@@ -7,6 +7,13 @@ const customTheme: MantineThemeOverride = {
   headings: {
     fontFamily: FONT,
   },
+  colors: {
+    gray: ['#FFFFFF', '#F5F5F6', '#EAEBED', '#D5D6DC', '#ACADB9', '#7B7C88', '#232134'],
+    blue: ['#DEECFF', '#C9E0FF', '#B7D6FF', '#92C1FF', '#5E96FC', '#5E96FC'],
+  },
+  primaryColor: 'blue',
+  primaryShade: 4,
+  black: '#232134',
 };
 
 export default customTheme;
