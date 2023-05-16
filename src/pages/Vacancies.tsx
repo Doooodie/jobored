6b1e947ce3fa3528d5959a71d64ca4aa98ccc1a9
Doooodie <unfair.slenderman@gmail.com>
2023-05-16 17:45:06 +0200

    feat: add search input ui

--- src/pages/Vacancies.tsx
+++ src/pages/Vacancies.tsx
@@ -1,5 +1,31 @@
+import { Box, Button, Container, Flex, Paper, TextInput } from '@mantine/core';
+import { ReactComponent as SearchIcon } from 'assets/icons/search.svg';
+
 function Vacancies() {
-  return <h1>Вакансии</h1>;
+  return (
+    <Container size={1148}>
+      <Flex gap={28} wrap='wrap' justify='center'>
+        <Paper component='aside' w={315} h={360} radius={12}>
+          Фильтры
+        </Paper>
+        <Box component='section' w={773}>
+          <TextInput
+            aria-label='Поиск'
+            radius='md'
+            icon={<SearchIcon />}
+            placeholder='Введите название вакансии'
+            rightSection={
+              <Button radius='md' h={32}>
+                Поиск
+              </Button>
+            }
+            rightSectionWidth={107}
+            styles={{ input: { height: '3rem' } }}
+          />
+        </Box>
+      </Flex>
+    </Container>
+  );
 }
 
 export default Vacancies;
