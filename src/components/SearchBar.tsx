1b4572092b25c1d324428503905900cab6936d84
Doooodie <unfair.slenderman@gmail.com>
2023-05-19 19:03:44 +0200

    fix: search bar submit

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,24 +8,26 @@ function SearchBar() {
   const ref = useRef<HTMLInputElement>(null);
   const dispatch = useAppDispatch();
 
-  const onSubmit = () => {
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
     dispatch(changeForm({ keyword: ref?.current?.value }));
   };
 
   return (
-    <TextInput
-      ref={ref}
-      aria-label='Поиск'
-      data-elem='search-input'
-      icon={<SearchIcon />}
-      placeholder='Введите название вакансии'
-      onSubmit={onSubmit}
-      rightSection={
-        <Button h={32} data-elem='search-button'>
-          Поиск
-        </Button>
-      }
-    />
+    <form onSubmit={onSubmit}>
+      <TextInput
+        ref={ref}
+        aria-label='Поиск'
+        data-elem='search-input'
+        icon={<SearchIcon />}
+        placeholder='Введите название вакансии'
+        rightSection={
+          <Button type='submit' h={32} data-elem='search-button'>
+            Поиск
+          </Button>
+        }
+      />
+    </form>
   );
 }
 
