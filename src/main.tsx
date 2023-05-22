eadd73db0d47d86a4d0797598ba9c6ed5cd69132
Doooodie <unfair.slenderman@gmail.com>
2023-05-22 22:46:12 +0200

    feat: save favorites to localstorage

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import store from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from 'redux/store';
 import browserRouter from 'utils/browserRouter';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={browserRouter} />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={browserRouter} />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,16 @@
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-
+import createFilter from 'redux-persist-transform-filter';
+import storage from 'redux-persist/lib/storage';
 import { jobsApi } from './api/jobsApi';
 import formReducer from './slices/formSlice';
 import favoritesReducer from './slices/favoritesSlice';
@@ -10,11 +21,24 @@ const rootReducer = combineReducers({
   [jobsApi.reducerPath]: jobsApi.reducer,
 });
 
+const saveSubsetFilter = createFilter('favorites', ['ids']);
+const persistedReducer = persistReducer(
+  { key: 'root', storage, whitelist: ['favorites'], transforms: [saveSubsetFilter] },
+  rootReducer
+);
+
 const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(jobsApi.middleware),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(jobsApi.middleware),
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
